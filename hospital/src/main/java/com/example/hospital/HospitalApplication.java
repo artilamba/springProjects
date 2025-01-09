363713147d497063fb4a6146e32d7c0a166e7963
Arti <arti@monocept.com>
2025-01-09 17:10:51 +0530

    New Class added

--- hospital/src/main/java/com/example/hospital/HospitalApplication.java
+++ hospital/src/main/java/com/example/hospital/HospitalApplication.java
@@ -8,8 +8,7 @@ public class HospitalApplication {
 
 	public static void main(String[] args) {
 
-		System.out.println("hello world");
-		System.out.println("hello world");
+
 
 		SpringApplication.run(HospitalApplication.class, args);
 	}
---
+++ hospital/src/main/java/com/example/hospital/TestClass.java
@@ -0,0 +1,8 @@
+package com.example.hospital;
+
+public class TestClass {
+
+    public static void main(String[] args) {
+        System.out.println("for testing purpose,,,,");
+    }
+}
